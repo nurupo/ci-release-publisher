ea2fb10eb68e1ac497a29d7704982c8fa7ef9f6c
Maxim Biro <nurupo.contributions@gmail.com>
2019-05-29 22:59:34 -0400

    v0.1.0

--- ci_release_publisher/__version__.py
+++ ci_release_publisher/__version__.py
@@ -2,7 +2,7 @@
 
 __title__ = 'ci_release_publisher'
 __description__ = 'A script for publishing Travis-CI build artifacts on GitHub Releases'
-__version__ = '0.1.0rc1'
+__version__ = '0.1.0'
 __url__ = 'https://github.com/nurupo/ci-release-publisher'
 __author__ = 'Maxim Biro'
 __author_email__ = 'nurupo.contributions@gmail.com'
