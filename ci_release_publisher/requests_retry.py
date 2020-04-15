cd719730b1064f249f3c3af1c8b9cf3954754845
Maxim Biro <nurupo.contributions@gmail.com>
2020-04-15 02:57:37 -0400

    Add an option to show response on retry exception

--- ci_release_publisher/requests_retry.py
+++ ci_release_publisher/requests_retry.py
@@ -4,10 +4,29 @@ from requests.adapters import HTTPAdapter
 import requests
 
 from . import config
+from . import env
 
 def requests_retry():
     session = requests.Session()
-    adapter = HTTPAdapter(max_retries=config.retries())
+    retry = config.retries()
+
+    debug = env.optional('CIRP_DEBUG')
+    if debug == '1':
+        retry.raise_on_status=False
+        def add_response_to_exeption(session, fn_name):
+            fn_old = getattr(session, fn_name)
+            def fn_new(*args, **kwargs):
+                try:
+                    r = fn_old(*args, **kwargs)
+                    r.raise_for_status()
+                    return r
+                except requests.exceptions.HTTPError as e:
+                    raise type(e)('{}\n{}\n{}'.format(r.headers, r.text, str(e)))
+        add_response_to_exeption(session, 'get')
+        add_response_to_exeption(session, 'post')
+
+    adapter = HTTPAdapter(max_retries=retry)
     session.mount('http://', adapter)
     session.mount('https://', adapter)
+
     return session
